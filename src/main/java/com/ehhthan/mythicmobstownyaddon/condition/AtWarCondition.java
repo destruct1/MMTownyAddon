5d8a61ec7862eb8889be1c0baf768ee6ffdad82d
ethan <ethan@plexiate.com>
2021-09-22 15:54:33 -0400

    update condition

--- src/main/java/com/ehhthan/mythicmobstownyaddon/condition/AtWarCondition.java
+++ src/main/java/com/ehhthan/mythicmobstownyaddon/condition/AtWarCondition.java
@@ -1,27 +1,47 @@
 package com.ehhthan.mythicmobstownyaddon.condition;
 
+import com.palmergames.bukkit.towny.TownyAPI;
+import com.palmergames.bukkit.towny.object.Nation;
+import com.palmergames.bukkit.towny.object.Resident;
+import com.palmergames.bukkit.towny.object.Town;
 import io.lumine.xikage.mythicmobs.adapters.AbstractEntity;
 import io.lumine.xikage.mythicmobs.adapters.bukkit.BukkitAdapter;
 import io.lumine.xikage.mythicmobs.io.MythicLineConfig;
 import io.lumine.xikage.mythicmobs.skills.ITargetedEntitySkill;
 import io.lumine.xikage.mythicmobs.skills.SkillCondition;
 import io.lumine.xikage.mythicmobs.skills.SkillMetadata;
+import io.lumine.xikage.mythicmobs.skills.conditions.IEntityComparisonCondition;
 import io.lumine.xikage.mythicmobs.skills.conditions.IEntityCondition;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.Nullable;
 
-public class AtWarCondition extends SkillCondition implements ITargetedEntitySkill {
+public class AtWarCondition extends SkillCondition implements IEntityComparisonCondition {
+    private final TownyAPI townyAPI;
+
     public AtWarCondition(MythicLineConfig config) {
         super(config.getLine());
+        this.townyAPI = TownyAPI.getInstance();
     }
 
     @Override
-    public boolean check(AbstractEntity target) {
-        LivingEntity bukkitTarget = (LivingEntity) BukkitAdapter.adapt(target);
-        return (bukkitTarget.getVehicle() != null);
+    public boolean check(AbstractEntity abstractEntity, AbstractEntity abstractTarget) {
+        Entity entity = abstractEntity.getBukkitEntity();
+        Entity target = abstractTarget.getBukkitEntity();
+        if (entity instanceof Player && target instanceof Player) {
+            Nation entityNation = townyAPI.getResidentNationOrNull(townyAPI.getResident((Player) entity));
+            Nation targetNation = townyAPI.getResidentNationOrNull(townyAPI.getResident((Player) target));
+            if (townyAPI.)
+
+        } else
+            return true;
     }
 
-    @Override
-    public boolean castAtEntity(SkillMetadata skillMetadata, AbstractEntity abstractEntity) {
-        return false;
+
+    public boolean atWar(Nation first, Nation second) {
+        if (first == null || second == null)
+            return false;
+        townyAPI.
     }
 }
