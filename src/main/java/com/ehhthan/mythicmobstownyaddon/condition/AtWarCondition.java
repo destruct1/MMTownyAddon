8b7d8316ee702585696716889f101856e8853e77
destruct1 <Destruct500@gmail.com>
2022-04-09 15:01:09 -0400

    Update AtWarCondition.java

--- src/main/java/com/ehhthan/mythicmobstownyaddon/condition/AtWarCondition.java
+++ src/main/java/com/ehhthan/mythicmobstownyaddon/condition/AtWarCondition.java
@@ -4,10 +4,10 @@ import com.palmergames.bukkit.towny.TownyAPI;
 import com.palmergames.bukkit.towny.TownyUniverse;
 import com.palmergames.bukkit.towny.object.Nation;
 import com.palmergames.bukkit.towny.war.eventwar.War;
-import io.lumine.xikage.mythicmobs.adapters.AbstractEntity;
-import io.lumine.xikage.mythicmobs.io.MythicLineConfig;
-import io.lumine.xikage.mythicmobs.skills.SkillCondition;
-import io.lumine.xikage.mythicmobs.skills.conditions.IEntityComparisonCondition;
+import io.lumine.mythic.api.adapters.AbstractEntity;
+import io.lumine.mythic.api.config.MythicLineConfig;
+import io.lumine.mythic.core.skills.SkillCondition;
+import io.lumine.mythic.api.skills.conditions.IEntityComparisonCondition;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 
