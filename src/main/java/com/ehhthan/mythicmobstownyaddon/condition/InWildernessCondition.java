b7ec7576611a37d867a599eee5292aeee6a82abc
destruct1 <Destruct500@gmail.com>
2022-04-09 15:02:36 -0400

    Update InWildernessCondition.java

--- src/main/java/com/ehhthan/mythicmobstownyaddon/condition/InWildernessCondition.java
+++ src/main/java/com/ehhthan/mythicmobstownyaddon/condition/InWildernessCondition.java
@@ -1,11 +1,11 @@
 package com.ehhthan.mythicmobstownyaddon.condition;
 
 import com.palmergames.bukkit.towny.TownyAPI;
-import io.lumine.xikage.mythicmobs.adapters.AbstractLocation;
-import io.lumine.xikage.mythicmobs.adapters.bukkit.BukkitAdapter;
-import io.lumine.xikage.mythicmobs.io.MythicLineConfig;
-import io.lumine.xikage.mythicmobs.skills.SkillCondition;
-import io.lumine.xikage.mythicmobs.skills.conditions.ILocationCondition;
+import io.lumine.mythic.api.adapters.AbstractLocation;
+import io.lumine.mythic.bukkit.BukkitAdapter;
+import io.lumine.mythic.api.config.MythicLineConfig;
+import io.lumine.mythic.core.skills.SkillCondition;
+import io.lumine.mythic.api.skills.conditions.ILocationCondition;
 
 public class InWildernessCondition extends SkillCondition implements ILocationCondition {
     private final TownyAPI townyAPI;
